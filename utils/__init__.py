90030137916cc4d2a8a97415c5de1c63f7cfbc71
vaishnavnegi <vaishnavnegi207@gmail.com>
2024-05-06 12:46:47 +0200

    Added functions in tracker for adding annotations, ellipses, and other visual markers for different object types. Also added a few utility functions in bbox_utils for support tasks of making these annotations.

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,3 @@
 #__init__.py exposes functions and files from inside utils to outside files.
 from .video_utils import read_video, save_video
+from .bbox_utils import get_center_of_bbox, get_bbox_width, get_foot_position, measure_distance, measure_xy_distance
