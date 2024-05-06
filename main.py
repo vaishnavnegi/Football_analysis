9dc4162b45178c526c72f991fac5cc680417d1c2
vaishnavnegi <vaishnavnegi207@gmail.com>
2024-05-06 14:05:13 +0200

    Wrote the modularized code for team assignment based on jersey colors, modified tracker accordingly, tested through main.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from utils import read_video, save_video
 from trackers import Tracker
 import cv2
+from team_assigner import TeamAssigner
 
 def main():
     #Read video
@@ -23,9 +24,23 @@ def main():
         #saving the image
         cv2.imwrite(f'output_videos/cropped_image.jpg', cropped_image) """
     
+    # Assign Player Teams
+    team_assigner = TeamAssigner()
+    team_assigner.assign_team_color(video_frames[0], 
+                                    tracks['players'][0])
+    
+    for frame_num, player_track in enumerate(tracks['players']):
+        for player_id, track in player_track.items():
+            team = team_assigner.get_player_team(video_frames[frame_num],   
+                                                 track['bbox'],
+                                                 player_id)
+            tracks['players'][frame_num][player_id]['team'] = team 
+            tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
+
+    
     #Draw output
     ##Drawing object tracks
-    output_video_frames = tracker.draw_annotations(video_frames, tracks)
+    #output_video_frames = tracker.draw_annotations(video_frames, tracks)
     
     #Save Video
     save_video(video_frames, 'output_videos/op_vdo.avi')
